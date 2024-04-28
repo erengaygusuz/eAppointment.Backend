3a1a5350c71cca9bf1a65c5300ffd9f893429b7a
erengaygusuz <gaygusuzeren@gmail.com>
2024-04-28 10:34:10 +0300

    ApplicationDbContext is Added and Configured

---
+++ eAppointment.Backend.Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using eAppointment.Backend.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace eAppointment.Backend.Infrastructure.Context
+{
+    internal sealed class ApplicationDbContext : IdentityDbContext<AppUser, AppRole, Guid>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+    }
+}
--- eAppointment.Backend.Infrastructure/DependencyInjection.cs
+++ eAppointment.Backend.Infrastructure/DependencyInjection.cs
@@ -1,11 +1,31 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using eAppointment.Backend.Domain.Entities;
+using eAppointment.Backend.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace eAppointment.Backend.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("SqlServer"));
+            });
+
+            services.AddIdentity<AppUser, AppRole>(action =>
+            {
+
+                action.Password.RequiredLength = 1;
+                action.Password.RequireUppercase = false;
+                action.Password.RequireLowercase = false;
+                action.Password.RequireNonAlphanumeric = false;
+                action.Password.RequireDigit = false;
+
+            }).AddEntityFrameworkStores<ApplicationDbContext>();
+
             return services;
         }
     }
