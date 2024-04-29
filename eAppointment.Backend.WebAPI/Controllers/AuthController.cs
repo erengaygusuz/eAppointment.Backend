c88557a86ea5a036402ef12bfd90e12cf454d46a
erengaygusuz <gaygusuzeren@gmail.com>
2024-04-29 16:39:05 +0300

    Abstract ApiController Class is Added

--- eAppointment.Backend.WebAPI/Controllers/AuthController.cs
+++ eAppointment.Backend.WebAPI/Controllers/AuthController.cs
@@ -1,17 +1,20 @@
 ﻿using eAppointment.Backend.Application.Features.Auth.Login;
+using eAppointment.Backend.WebAPI.Abstractions;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace eAppointment.Backend.WebAPI.Controllers
 {
-    [Route("api/[controller]/[action]")]
-    [ApiController]
-    public class AuthController (IMediator mediator): ControllerBase
+    public sealed class AuthController : ApiController
     {
+        public AuthController(IMediator mediator) : base(mediator)
+        {
+        }
+
         [HttpPost]
         public async Task<IActionResult> Login(LoginCommand request, CancellationToken cancellationToken)
         {
-            var response = await mediator.Send(request, cancellationToken);
+            var response = await _mediator.Send(request, cancellationToken);
 
             return StatusCode(response.StatusCode, response);
         }
