db6b042501a975c0bf223c47b94dcd9ed8c83022
erengaygusuz <gaygusuzeren@gmail.com>
2024-07-21 10:16:56 +0300

    AppointmentStatus Enum Values are Reconfigured

--- eAppointment.Backend.Domain/Enums/AppointmentStatus.cs
+++ eAppointment.Backend.Domain/Enums/AppointmentStatus.cs
@@ -4,10 +4,10 @@ namespace eAppointment.Backend.Domain.Enums
 {
     public sealed class AppointmentStatus : SmartEnum<AppointmentStatus>
     {
-        public static readonly AppointmentStatus SuccessfullyCompleted = new("Successfully Completed", 1);
-        public static readonly AppointmentStatus NotAttended = new("Not Attended", 2);
+        public static readonly AppointmentStatus SuccessfullyCompleted = new("SuccessfullyCompleted", 1);
+        public static readonly AppointmentStatus NotAttended = new("NotAttended", 2);
         public static readonly AppointmentStatus Cancelled = new("Cancelled", 3);
-        public static readonly AppointmentStatus NotCompleted = new("Not Completed", 4);
+        public static readonly AppointmentStatus NotCompleted = new("NotCompleted", 4);
 
         public AppointmentStatus(string name, int value) : base(name, value)
         {
