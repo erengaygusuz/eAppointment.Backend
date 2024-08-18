46ef063f769553ef7cfd8763b51713a32360b4b8
erengaygusuz <gaygusuzeren@gmail.com>
2024-08-18 13:45:18 +0300

    Log Settings Configured on CreateAdminCommandHandler

--- eAppointment.Backend.Application/Features/Admins/CreateAdmin/CreateAdminCommandHandler.cs
+++ eAppointment.Backend.Application/Features/Admins/CreateAdmin/CreateAdminCommandHandler.cs
@@ -5,7 +5,9 @@ using GenericRepository;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
 using System.Data;
+using System.Text.Json;
 using TS.Result;
 
 namespace eAppointment.Backend.Application.Features.Admins.CreateAdmin
@@ -15,38 +17,63 @@ namespace eAppointment.Backend.Application.Features.Admins.CreateAdmin
         IUnitOfWork unitOfWork,
         IMapper mapper,
         IValidator<CreateAdminCommand> createAdminCommandValidator,
-        IStringLocalizer<object> localization) : IRequestHandler<CreateAdminCommand, Result<string>>
+        IStringLocalizer<object> localization,
+        ILogger<CreateAdminCommandHandler> logger) : IRequestHandler<CreateAdminCommand, Result<string>>
     {
         public async Task<Result<string>> Handle(CreateAdminCommand request, CancellationToken cancellationToken)
         {
             var translatedMessagePath = "Features.Admins.CreateAdmin.Others";
 
+            logger.LogInformation("Validation Started");
+
             var validationResult = await createAdminCommandValidator.ValidateAsync(request);
 
             if (!validationResult.IsValid)
             {
+                logger.LogError("Validation Completed with Errors: ", JsonSerializer.Serialize(validationResult.Errors.Select(x => x.ErrorMessage).ToList()));
+
                 return Result<string>.Failure(validationResult.Errors.Select(x => x.ErrorMessage).ToList());
             }
 
+            logger.LogInformation("Validation Completed Successfully");
+
+            logger.LogInformation("Mapping Started");
+
             User user = mapper.Map<User>(request);
+
+            logger.LogInformation("Mapping Completed Successfully");
+
+            logger.LogInformation("User Creation Started");
 
             IdentityResult result = await userManager.CreateAsync(user, request.password);
 
             if (!result.Succeeded)
             {
+                logger.LogError(localization[translatedMessagePath + "." + "CannotCreated"].Value);
+
                 return Result<string>.Failure(localization[translatedMessagePath + "." + "CannotCreated"]);
             }
 
+            logger.LogInformation("User Creation Completed Successfully");
+
             var addedUser = await userManager.FindByEmailAsync(user.Email!);
+
+            logger.LogInformation("User Role Addition Started");
 
             var roleResult = await userManager.AddToRoleAsync(addedUser!, "Admin");
 
             if (!roleResult.Succeeded)
             {
+                logger.LogError(localization[translatedMessagePath + "." + "RoleCannotAdded"].Value);
+
                 return Result<string>.Failure(localization[translatedMessagePath + "." + "RoleCannotAdded"]);
             }
 
+            logger.LogInformation("User Role Addition Completed Successfully");
+
             await unitOfWork.SaveChangesAsync(cancellationToken);
+
+            logger.LogInformation(localization[translatedMessagePath + "." + "SuccessfullyCreated"].Value);
 
             return Result<string>.Succeed(localization[translatedMessagePath + "." + "SuccessfullyCreated"].Value);
         }
